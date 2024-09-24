380d31678d8dcb83c7ca32c3be209bcd355cd4fb
Leticia Alves de Pontes <50830736+leticia-hub@users.noreply.github.com>
2024-09-24 12:43:15 -0300

    Atualizando .gitignore/Alterando url de acesso principal

--- termooo/urls.py
+++ termooo/urls.py
@@ -4,9 +4,9 @@ from django.conf import settings
 from django.conf.urls.static import static
 
 urlpatterns = [
-    path('palavra/', include('palavra.urls')),
     path('admin/', admin.site.urls),
+    path('', include('palavra.urls')),
 ]
 
-if settings.DEBUG:
-    urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+# if settings.DEBUG:
+#     urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
