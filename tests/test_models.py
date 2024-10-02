191f0e41612e51cbd50c78a93c0ac201ffb59333
Leticia Alves de Pontes <50830736+leticia-hub@users.noreply.github.com>
2024-10-02 01:09:39 -0300

    Criando e corrigindo testes/Verificando coverage

---
+++ tests/test_models.py
@@ -0,0 +1,45 @@
+from django.test import TestCase
+from django.core.files.uploadedfile import SimpleUploadedFile
+from palavra.models import Usuario, Tema, Palavra
+
+class UsuarioModelTest(TestCase):
+    
+    def setUp(self):
+        self.usuario = Usuario(username='testuser', password='testpass', pontuacao_total=100)
+        self.usuario.save()
+
+    def test_str(self):
+        self.assertEqual(str(self.usuario), 'testuser')
+
+    def test_foto_perfil(self):
+
+        image = SimpleUploadedFile(
+            name='admir.jpg',
+            content=b'fake image content',
+            content_type='image/jpeg'
+        )
+        
+        self.usuario.foto_perfil.save('admir.jpg', image)
+
+        self.assertIsNotNone(self.usuario.foto_perfil)
+        self.assertTrue(self.usuario.foto_perfil.name.startswith('palavra/foto_perfil/admir'))
+
+
+class TemaModelTest(TestCase):
+    def setUp(self):
+        self.tema = Tema.objects.create(descricao='Teste')
+
+    def test_str(self):
+        self.assertEqual(str(self.tema), 'Teste')
+
+class PalavraModelTest(TestCase):
+    def setUp(self):
+        self.tema = Tema.objects.create(descricao='Teste')
+        self.palavra = Palavra.objects.create(descricao='palavra', dificuldade=Palavra.FACIL, tema=self.tema)
+
+    def test_str(self):
+        self.assertEqual(str(self.palavra), 'palavra')
+
+    def test_dificuldade_choices(self):
+        self.assertEqual(self.palavra.dificuldade, Palavra.FACIL)
+        self.assertIn(self.palavra.dificuldade, dict(Palavra.DIFICULDADE_CHOICES).keys())
